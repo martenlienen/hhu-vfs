db7fee159a317923778e88ecf6654626d9fd1f25
Marten Lienen <marten.lienen@gmail.com>
2013-07-28 01:24:51 +0200

    Write to the structure file

--- spec.rb
+++ spec.rb
@@ -1,12 +1,13 @@
 describe "VFS" do
   before(:each) do
-    `rm -f archive.structure archive.store`
+    `rm -rf tmp`
+    `mkdir tmp`
   end
 
   describe "Creating a new archive" do
     describe "under normal conditions" do
       before(:each) do
-        `./vfs archive create 2 4`
+        `./vfs ./tmp/archive create 2 4`
       end
 
       it "should succeed" do
@@ -14,22 +15,22 @@ describe "VFS" do
       end
 
       it "should create a .store file" do
-        expect(File.exists? "archive.store").to be_true
+        expect(File.exists? "./tmp/archive.store").to be_true
       end
 
       it "should create a .structure file" do
-        expect(File.exists? "archive.structure").to be_true
+        expect(File.exists? "./tmp/archive.structure").to be_true
       end
 
       it "should put BLOCKSIZE*BLOCKCOUNT bytes in the structure file" do
-        expect(File.size "archive.store").to eq 8
+        expect(File.size "./tmp/archive.store").to eq 8
       end
     end
 
     describe "when the archive already exists" do
       before(:each) do
-        `./vfs archive create 2 4`
-        `./vfs archive create 2 4`
+        `./vfs ./tmp/archive create 2 4`
+        `./vfs ./tmp/archive create 2 4`
       end
 
       it "should exit with code 3" do
@@ -41,33 +42,40 @@ describe "VFS" do
   describe "Reading and writing" do
     describe "writing files" do
       it "should exit with code 2 when the vfs is not readable" do
-        `./vfs archive add vfs.c vfs.c`
+        `./vfs ./tmp/archive add vfs.c vfs.c`
 
         expect($?.exitstatus).to eq 2
       end
 
       describe "when writing to a valid archive" do
         before(:each) do
-          `./vfs archive create 4096 4`
+          `./vfs ./tmp/archive create 4096 4`
         end
 
         it "should exit with code 0 when the file has been added" do
-          `./vfs archive add vfs.c vfs.c`
+          `./vfs ./tmp/archive add vfs.c vfs.c`
 
           expect($?.exitstatus).to eq 0
         end
 
         it "should exit with code 13 when the source file does not exist" do
-          `./vfs archive add ./tmp/does_not_exist hi`
+          `./vfs ./tmp/archive add ./tmp/does_not_exist hi`
 
           expect($?.exitstatus).to eq 13
         end
          
         it "should exit with code 11 when a file with the same name already exists" do
-          `./vfs archive add vfs.c vfs.c`
-          `./vfs archive add vfs.c vfs.c`
+          `./vfs ./tmp/archive add vfs.c vfs.c`
+          `./vfs ./tmp/archive add vfs.c vfs.c`
 
           expect($?.exitstatus).to eq 11
+        end
+        
+        it "should exit with code 12 when there is not enough space" do
+          `dd count=100000 if=/dev/null of=./tmp/big_file 2>&1 > /dev/null`
+          `./vfs ./tmp/archive add ./tmp/big_file too_big`
+
+          expect($?.exitstatus).to eq 12
         end
       end
     end
--- vfs.c
+++ vfs.c
@@ -5,6 +5,18 @@
 
 typedef unsigned long int ulint;
 
+bool file_exists (const char* file) {
+  FILE* handle;
+
+  if ((handle = fopen(file, "r"))) {
+    fclose(handle);
+
+    return true;
+  } else {
+    return false;
+  }
+}
+
 struct FileInfo {
   /**
    * Dateiname
@@ -22,7 +34,7 @@ void fileinfo_free (struct FileInfo* file_info) {
   free(file_info);
 }
 
-struct Archive {
+struct ArchiveInfo {
   /**
    * Größe eines Blocks in Bytes
    */
@@ -47,125 +59,213 @@ struct Archive {
   struct FileInfo** file_infos;
 }; 
 
-struct Archive* archive_create_empty (ulint blocksize, ulint blockcount) {
-  struct Archive* archive = malloc(sizeof(struct Archive));
-  archive->blocksize = blocksize;
-  archive->blockcount = blockcount;
-  archive->blocks = malloc(blockcount * sizeof(int));
-  archive->num_files = 0;
-  archive->file_infos = NULL;
+struct ArchiveInfo* archive_info_create_empty (ulint blocksize, ulint blockcount) {
+  struct ArchiveInfo* archive_info = malloc(sizeof(struct ArchiveInfo));
+  archive_info->blocksize = blocksize;
+  archive_info->blockcount = blockcount;
+  archive_info->blocks = malloc(blockcount * sizeof(int));
+  archive_info->num_files = 0;
+  archive_info->file_infos = NULL;
 
-  memset(archive->blocks, -1, blockcount);
+  memset(archive_info->blocks, -1, blockcount);
 
-  return archive;
+  return archive_info;
 }
 
 /**
  * Speichert das Archiv in eine Strukturdatei.
  */
-int archive_save (struct Archive* archive, FILE* file) {
+int archive_info_save (struct ArchiveInfo* archive_info, FILE* file) {
   return 0;
+}
+
+void archive_info_free (struct ArchiveInfo* archive_info) {
+  free(archive_info->blocks);
+
+  int i;
+  for (i = 0; i < archive_info->num_files; i++) {
+    fileinfo_free(archive_info->file_infos[i]);
+  }
+
+  free(archive_info->file_infos);
+
+  free(archive_info); 
+}
+
+#define ARCHIVE_ALREADY_EXISTS 1
+#define ARCHIVE_NOT_WRITEABLE 2
+
+/**
+ * Ein High-Level-Interface um mit einem Archiv zu interagieren.
+ */
+struct Archive {
+  char* structure_file;
+  char* store_file;
+  struct ArchiveInfo* archive_info;
+};
+
+/**
+ * Erstellt ein leeres Archiv, das dann mit einer der Initializer-Methoden
+ * geladen werden muss.
+ */
+struct Archive* archive_create () {
+  struct Archive* archive = malloc(sizeof(struct Archive));
+  archive->structure_file = NULL;
+  archive->store_file = NULL;
+  archive->archive_info = NULL;
+
+  return archive;
+}
+
+int archive_write_archive_info (struct Archive*);
+int archive_initialize_store(struct Archive*);
+int archive_fwrite(const void*, int, int, FILE*);
+
+/**
+ * Initialisiert ein leeres Archiv.
+ */
+int archive_initialize_empty (struct Archive* archive, const char* structure_file, const char* store_file, ulint blocksize, ulint blockcount) {
+  archive->structure_file = malloc((strlen(structure_file) + 1) * sizeof(char));
+  strcpy(archive->structure_file, structure_file);
+
+  archive->store_file = malloc((strlen(store_file) + 1) * sizeof(char));
+  strcpy(archive->store_file, store_file);
+
+  archive->archive_info = archive_info_create_empty(blocksize, blockcount);
+
+  int status = 0;
+
+  if (file_exists(archive->store_file) || file_exists(archive->structure_file)) {
+    status = ARCHIVE_ALREADY_EXISTS;
+  }
+
+  status == 0 && (status = archive_write_archive_info(archive));
+  status == 0 && (status = archive_initialize_store(archive));
+
+  return status;
 }
 
 /**
- * Initialisiert die Datendatei mit Nullen.
+ * Schreibt die Strukturdatei neu.
+ *
+ * @private
  */
-int archive_init_data_file (struct Archive* archive, FILE* file) {
-  const char* zero_block[archive->blocksize];
-  memset(&zero_block, 0, archive->blocksize);
-
-  int i;
-  for (i = 0; i < archive->blockcount; i++) {
-    fwrite(zero_block, sizeof(char), archive->blocksize, file);
-  }
-
-  return 0;
-}
-
-void archive_free (struct Archive* archive) {
-  free(archive->blocks);
-
-  int i;
-  for (i = 0; i < archive->num_files; i++) {
-    fileinfo_free(archive->file_infos[i]);
-  }
-
-  free(archive->file_infos);
-
-  free(archive); 
-}
-
-bool file_exists (const char* file) {
-  FILE* handle;
-
-  if (handle = fopen(file, "r")) {
-    fclose(handle);
-
-    return true;
-  } else {
-    return false;
-  }
-}
-
-int cli_create (char* structure_path, char* store_path, ulint blocksize, ulint blockcount) {
+int archive_write_archive_info (struct Archive* archive) {
   int status = 0;
 
-  if (file_exists(store_path) || file_exists(structure_path)) {
-    printf("Das Archiv existiert bereits");
+  FILE* structure_file = fopen(archive->structure_file, "w");
 
-    status = 3;
+  if (structure_file == NULL) {
+    status = ARCHIVE_NOT_WRITEABLE;
   } else {
-    FILE* store_file = fopen(store_path, "a");
+    struct ArchiveInfo* archive_info = archive->archive_info;
 
-    if (store_file == NULL) {
-      printf("Das Archiv konnte nicht erstellt werden");
+    status == 0 && (status = archive_fwrite(archive_info, sizeof(ulint), 2, structure_file));
+    status == 0 && (status = archive_fwrite(archive_info->blocks, sizeof(int), archive_info->blockcount, structure_file));
+    status == 0 && (status = archive_fwrite(&archive_info->num_files, sizeof(int), 1, structure_file));
 
-      status = 1;
-    } else {
-      FILE* structure_file = fopen(structure_path, "a");
+    int i;
+    for (i = 0; i < archive_info->num_files && status == 0; i++) {
+      struct FileInfo* file_info = archive_info->file_infos[i]; 
 
-      if (structure_file == NULL) {
-        printf("Das Archiv konnte nicht erstellt werden");
-
-        if (remove(store_path) != 0) {
-          printf("Die bereits erstellte Datendatei konnte nicht gelöscht werden");
-        }
-        
-        status = 1;
-      } else {
-        struct Archive* archive = archive_create_empty(blocksize, blockcount);
-
-        if (archive_save(archive, structure_file) != 0) {
-          printf("Die Strukturdatei konnte nicht gespeichert werden");
-          status = 1;
-        } else if (archive_init_data_file(archive, store_file) != 0) {
-          printf("Die Datendatei konnte nicht erstellt werden");
-
-          if (remove(structure_path) != 0) {
-            printf("Die bereits erstellt Datendatei konnte nicht gelöscht werden");
-          }
-
-          status = 1;
-        }
-
-        archive_free(archive);
-      }
-
-      fclose(store_file);
+      archive_fwrite(file_info->name, sizeof(char), strlen(file_info->name) + 1, structure_file);
+      status == 0 && (status = archive_fwrite(&file_info->size, sizeof(int), 1, structure_file));
     }
+
+    if (status != 0) {
+      status = ARCHIVE_NOT_WRITEABLE;
+    }
+
+    fclose(structure_file);
+  }
+
+  return status; 
+}
+
+/**
+ * Schreibt in eine Datei und gibt bei Erfolg 0 zurück.
+ *
+ * @private
+ */
+int archive_fwrite (const void* data, int size, int num, FILE* file) {
+  fwrite(data, size, num, file);
+
+  if (ferror(file)) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+/**
+ * Initialisiert einen leeren Datenstore in eine nicht existente Datei.
+ *
+ * @private
+ */
+int archive_initialize_store (struct Archive* archive) {
+  int status = 0;
+
+  FILE* store = fopen(archive->store_file, "w");
+
+  if (store == NULL) {
+    status = ARCHIVE_NOT_WRITEABLE;
+  } else {
+    struct ArchiveInfo* archive_info = archive->archive_info;
+    const char* zero_block[archive_info->blocksize];
+    memset(&zero_block, 0, archive_info->blocksize);
+
+    ulint i;
+    for (i = 0; i < archive_info->blockcount; i++) {
+      fwrite(zero_block, sizeof(char), archive_info->blocksize, store);
+
+      if (ferror(store) != 0) {
+        status = ARCHIVE_NOT_WRITEABLE;
+      }
+    }
+
+    fclose(store);
   }
 
   return status;
 }
 
-int cli_add (const char* structure_path, const char* store_path, const char* sourcePath, const char* target) {
+/**
+ * Gibt alle belegten Resourcen frei.
+ */
+void archive_free (struct Archive* archive) {
+  free(archive->structure_file);
+  free(archive->store_file);
+  free(archive->archive_info);
+  free(archive);
+}
+
+int cli_create (char* structure_path, char* store_path, ulint blocksize, ulint blockcount) {
   int status = 0;
 
-  if (!file_exists(sourcePath)) {
+  struct Archive* archive = archive_create();
+  status = archive_initialize_empty(archive, structure_path, store_path, blocksize, blockcount);
+  archive_free(archive);
+
+  switch (status) {
+    case ARCHIVE_NOT_WRITEABLE:
+      printf("Das Archiv ist nicht beschreibbar");
+      return 1;
+    case ARCHIVE_ALREADY_EXISTS:
+      printf("Es existiert bereits ein Archiv mit dem selben Namen");
+      return 3;
+    default:
+      return 0;
+  }
+}
+
+int cli_add (const char* structure_path, const char* store_path, const char* source_path, const char* target) {
+  int status = 0;
+
+  if (!file_exists(source_path)) {
     printf("Quelldatei existiert nicht");
 
     status = 13;
-  }
+  } 
 
   return status;
 }
